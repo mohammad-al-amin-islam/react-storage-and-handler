df4c53df66dc84430cddb474540a0750a5b06407
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2022-03-21 11:06:19 +0600

    button with parameter added

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const { name, price, id } = props.cosmetic;
+    const addToCart = (id) => {
+        console.log(id)
+    }
+    // const addCartWithParam = () => addToCart(id);
     return (
-        <div>
+        <div className='product'>
             <h1>Product Name:{name}</h1>
             <h3>Price:{price}</h3>
             <h3>ID:{id}</h3>
-        </div>
+            <button onClick={() => addToCart(id)}>Add Item</button>
+        </div >
     );
 };
 
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -4,7 +4,7 @@ import Cosmetic from '../Cosmetic/Cosmetic';
 const Cosmetics = () => {
     const [cosmetics, setCosmetics] = useState([]);
     useEffect(() => {
-        fetch('https://cosmetic.free.beeceptor.com/')
+        fetch('data.json')
             .then(res => res.json())
             .then(data => setCosmetics(data));
     }, [])
